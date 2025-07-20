18bf3914b6a8138d2c0b13d22467b340f8b2be26
Bakhomious Basta <bbasta@externe.europ-assistance.com>
2025-07-20 22:54:42 +0200

    Backend init

---
+++ backend/src/main/java/com/tcs/dhv/DhvApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.dhv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhvApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tcs/dhv/DhvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.dhv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
