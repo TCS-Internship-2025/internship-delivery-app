c856eba2f61aef0290cfda44bb41990903734521
Abdelrahman Abdelaal <abdelrahman5abdelaal@gmail.com>
2025-07-23 22:12:08 +0200

    fix: remove enums

--- backend/src/main/java/com/tcs/dhv/enums/DeliveryType.java
+++
@@ -1,7 +0,0 @@
-package com.tcs.dhv.enums;
-
-public enum DeliveryType {
-    HOME,
-    PICKUP_POINT
-    // PARCEL_BOX // optional for now
-}
--- backend/src/main/java/com/tcs/dhv/enums/PaymentType.java
+++
@@ -1,6 +0,0 @@
-package com.tcs.dhv.enums;
-
-public enum PaymentType {
-    SENDER_PAYS,
-    RECIPIENT_PAYS
-}
