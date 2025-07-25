3d1605734384a86c044c42b87f600bd34aae3262
Abdelrahman Abdelaal <abdelrahman5abdelaal@gmail.com>
2025-07-25 18:01:26 +0200

    chore: remove unneeded methods from ParcelRepository

--- backend/src/main/java/com/tcs/dhv/repository/ParcelRepository.java
+++ backend/src/main/java/com/tcs/dhv/repository/ParcelRepository.java
@@ -4,15 +4,12 @@ import com.tcs.dhv.entity.Parcel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
 public interface ParcelRepository extends JpaRepository<Parcel, UUID> {
     Page<Parcel> findAllBySenderId(UUID userId, Pageable pageable);
+    // For public tracking
     Optional<Parcel> findByTrackingCode(String trackingCode);
-    boolean existsByTrackingCode(String trackingCode);
-    long countBySenderId(UUID userId);
 }
