e216091127ba972551643b8bc904e4839b343eba
dana <148772690+danaskr@users.noreply.github.com>
2025-07-25 17:09:31 +0200

    Feature/22 create required entities (#40)

---
+++ backend/src/main/java/com/tcs/dhv/enums/ParcelStatus.java
@@ -0,0 +1,12 @@
+package com.tcs.dhv.enums;
+
+public enum ParcelStatus {
+    CREATED,
+    PICKED_UP,
+    IN_TRANSIT,
+    OUT_FOR_DELIVERY,
+    DELIVERED,
+    CANCELLED,
+    DELIVERY_ATTEMPTED,
+    RETURNED_TO_SENDER
+}
