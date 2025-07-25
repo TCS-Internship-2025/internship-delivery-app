890ecd3fdae97be3d19bcdaa074cf68defffcadd
fatgod4937 <dergeczakos1208@gmail.com>
2025-07-25 09:48:40 +0200

    [add] responsive Timelines using useSmallScreen

--- frontend/src/components/Timeline.tsx
+++ frontend/src/components/Timeline.tsx
@@ -1,8 +1,10 @@
+import { useSmallScreen } from '@/hooks/useSmallScreen';
+
 import Timeline from '@mui/lab/Timeline';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
-import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineItem, { timelineItemClasses } from '@mui/lab/TimelineItem';
 import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import Typography from '@mui/material/Typography';
@@ -16,21 +18,45 @@ interface TimelineProp {
 }
 
 export default function TimelineComponent({ status }: TimelineProp) {
+  const isSmallScreen = useSmallScreen();
   return (
-    <Timeline sx={{ maxWidth: '50%' }}>
+    <Timeline
+      sx={
+        isSmallScreen
+          ? {
+              [`& .${timelineItemClasses.root}:before`]: {
+                flex: 0,
+                padding: '0',
+              },
+            }
+          : {}
+      }
+    >
       {status.map((value) => {
         return (
           <TimelineItem key={value.changeMessage}>
-            <TimelineOppositeContent color="textSecondary">
-              {new Date(value.changeDate).toLocaleDateString()}
-            </TimelineOppositeContent>
+            {!isSmallScreen ? (
+              <TimelineOppositeContent color="textSecondary">
+                {new Date(value.changeDate).toLocaleDateString()}
+              </TimelineOppositeContent>
+            ) : (
+              ''
+            )}
             <TimelineSeparator>
               <TimelineDot />
               {value.changeMessage !== 'Delivered' ? <TimelineConnector /> : ''}
             </TimelineSeparator>
-            <TimelineContent>
+            <TimelineContent variant="subtitle2">
               {value.changeMessage}
-              <Typography variant="subtitle2">{value.changeLocation}</Typography>
+              <br />
+              <Typography variant="caption">{value.changeLocation}</Typography>
+              {isSmallScreen ? (
+                <Typography variant="subtitle2" color="textSecondary">
+                  {new Date(value.changeDate).toLocaleDateString()}
+                </Typography>
+              ) : (
+                ''
+              )}
             </TimelineContent>
           </TimelineItem>
         );
