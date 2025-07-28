55ad9522bbf34cd11baff2d632c636582a2dbc99
makswilder <maksim.shcherbakov.sh@gmail.com>
2025-07-28 18:08:05 +0200

    fix(auth): corrected typo in registration response and fixed @Column annotation in User entity

--- backend/src/main/java/com/tcs/dhv/service/EmailService.java
+++ backend/src/main/java/com/tcs/dhv/service/EmailService.java
@@ -1,30 +1,30 @@
-package com.tcs.dhv.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
-
-@Service
-@RequiredArgsConstructor
-public class EmailService {
-
-    private final JavaMailSender mailSender;
-
-    private final SimpleMailMessage template;
-
-    public final void sendEmail(
-        String email,
-        String trackingNumber,
-        String link
-    ) {
-        final var message = template;
-        Assert.notNull(message.getText(), "Message must not be null;");
-        message.setText(String.format(template.getText(), trackingNumber, link));
-        message.setTo(email);
-
-        mailSender.send(message);
-    }
-
-}
+//package com.tcs.dhv.service;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.stereotype.Service;
+//import org.springframework.util.Assert;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class EmailService {
+//
+//    private final JavaMailSender mailSender;
+//
+//    private final SimpleMailMessage template;
+//
+//    public final void sendEmail(
+//        String email,
+//        String trackingNumber,
+//        String link
+//    ) {
+//        final var message = template;
+//        Assert.notNull(message.getText(), "Message must not be null;");
+//        message.setText(String.format(template.getText(), trackingNumber, link));
+//        message.setTo(email);
+//
+//        mailSender.send(message);
+//    }
+//
+//}
