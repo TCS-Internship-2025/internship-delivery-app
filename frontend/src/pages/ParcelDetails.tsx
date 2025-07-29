dd61b83f7489fc66595bbd6f3573b41f8e79d0fc
martonlora <74675688+martonlora@users.noreply.github.com>
2025-07-29 17:26:17 +0200

    Feature/fe 060 improve user dashboard (#86)
    * feat: changed cardlist to ag grid layout
    
    * feat: updated layout of parcels table
    
    * fix: deleted parcel list and item
    
    * feat: imporved parcel grid and added navigation to parcel details
    
    * feat: updated parcel details page
    
    * feat: added responsive layout to parcel pages
    
    * feat: added get request for parcels
    
    * fix: adjustments based on peer comments

--- frontend/src/pages/ParcelDetails.tsx
+++ frontend/src/pages/ParcelDetails.tsx
@@ -1,33 +1,55 @@
-import { useNavigate } from 'react-router-dom';
-import { getParcelChipData } from '../utils/parcelChipData.ts';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { useGetParcelById } from '@/apis/parcelGet';
 
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Chip from '@mui/material/Chip';
+import CircularProgress from '@mui/material/CircularProgress';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 
-interface ParcelItemProps {
-  parcelData?: {
-    parcelId?: number;
-    address?: string;
-    delivery?: string;
-    payment?: string;
-    status: string;
-  };
-}
+import { ParcelDetailsContent } from '@/components/ParcelDetailsContent';
 
-export const ParcelDetails = ({ parcelData }: ParcelItemProps) => {
+export const ParcelDetails = () => {
+  const { id } = useParams();
+  const { data, status } = useGetParcelById(id);
   const navigate = useNavigate();
-  const parcelChipData = getParcelChipData(parcelData?.status);
+
+  if (status === 'pending') {
+    return (
+      <Box display={'flex'} justifyContent={'center'} alignItems={'center'} height={'100%'} mt={10}>
+        <CircularProgress />
+      </Box>
+    );
+  }
+  if (status === 'error') {
+    return (
+      <Box
+        display={'flex'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        height={'100%'}
+        flexDirection={'column'}
+        mt={10}
+      >
+        <Typography variant="h4">Something went wrong.</Typography>
+        <Typography variant="subtitle1">Please try again later!</Typography>
+      </Box>
+    );
+  }
 
   function handleBack() {
     void navigate('..');
   }
 
+  function handleDelete() {
+    // TODO: delete request
+    console.log('deleted');
+  }
+
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-      <Box sx={{ flex: 1, overflow: 'auto', minHeight: 0 }}>
+      <Box sx={{ flex: 1, overflow: 'auto' }}>
         <Container
           maxWidth={false}
           sx={{
@@ -37,31 +59,25 @@ export const ParcelDetails = ({ parcelData }: ParcelItemProps) => {
             flexDirection: 'column',
           }}
         >
-          <Typography variant="h3" ml={2} mt={1} mb={3}>
-            Test Parcel {parcelData?.parcelId}
-          </Typography>
-          <Typography variant="body1" fontSize={28} ml={6}>
-            Address: {parcelData?.address}
-          </Typography>
-          <Typography variant="body1" fontSize={28} ml={6}>
-            Delivery type: {parcelData?.delivery}
-          </Typography>
-          <Typography variant="body1" fontSize={28} ml={6}>
-            Payment type: {parcelData?.payment}
-          </Typography>
-          <Chip
-            color={parcelChipData.color}
-            label={parcelChipData.label}
-            sx={{ alignSelf: 'flex-start', fontSize: 24, padding: 3, mt: 6, ml: 10 }}
-          />
-          <Button
-            variant="outlined"
-            color="primary"
-            onClick={handleBack}
-            sx={{ alignSelf: 'center', py: 2, px: 4, mt: 15, fontSize: 20, borderRadius: 3 }}
-          >
-            Back
-          </Button>
+          <ParcelDetailsContent parcelData={data} />
+          <Box alignSelf="center" mt={{ xs: 8, md: 15 }}>
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={handleBack}
+              sx={{ py: 2, px: 4, mr: 2, fontSize: 20, borderRadius: 3 }}
+            >
+              Back
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleDelete}
+              sx={{ py: 2, px: 4, ml: 2, fontSize: 20, borderRadius: 3 }}
+            >
+              Delete
+            </Button>
+          </Box>
         </Container>
       </Box>
     </Box>
