fd8a3d7089f244275017994d885924461b0dd245
Roland Borsodi <borroldan@gmail.com>
2025-07-30 12:51:56 +0200

    Fix: Minor fixes and added recipient titles

---
+++ backend/src/main/resources/db/migration/V6__recipient_titles.sql
@@ -0,0 +1,3 @@
+CREATE TYPE recipient_title AS ENUM ('Mr', 'Ms', 'Mrs', 'Miss', 'Dr', 'Prof', 'Mx', 'Sir', 'Madam', 'Rev', 'Capt', 'Major', 'Col', 'Lt', 'Fr', 'Sr');
+
+ALTER TABLE recipient ADD COLUMN title recipient_title;
