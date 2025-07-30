7a02788da1a998e487f2541d4ee6420588644a56
Njoura7 <njourawebdev@gmail.com>
2025-07-30 12:55:11 +0200

    merge AuthContext and useAuth for simplicity

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,4 +1,10 @@
-import { createContext } from 'react';
+import { createContext, useContext } from 'react';
 import { type AuthContextType } from '@/types/auth';
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error('useAuth must be used within an AuthProvider');
+  return context;
+};
 
 export const AuthContext = createContext<AuthContextType | undefined>(undefined);
