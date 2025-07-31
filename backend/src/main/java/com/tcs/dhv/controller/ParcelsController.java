8c804a8b54710fe3ac0eed30e8b23b4a82409102
Abdelrahman Abdelaal <abdelrahman5abdelaal@gmail.com>
2025-07-31 19:51:06 +0200

    chore: change address change endpoint path

--- backend/src/main/java/com/tcs/dhv/controller/ParcelsController.java
+++ backend/src/main/java/com/tcs/dhv/controller/ParcelsController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -76,7 +77,7 @@ public class ParcelsController {
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/{id}/address-change")
+    @PutMapping("/{id}/address")
     public ResponseEntity<Void> changeAddress(
         @PathVariable final UUID id,
         @Valid @RequestBody final AddressChangeDto requestDto,
