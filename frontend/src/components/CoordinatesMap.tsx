f3adb9eb3e3fdb4f758366f0f5979b0d55905b96
Dézsenyi Balázs <75077702+cimpa2004@users.noreply.github.com>
2025-08-01 11:26:25 +0200

    Feature/fe 133 map implementation in user dashboard (#135)
    * ✨feature: added parcel box option to map
    
    * ✨feature: MapBox added to parceldeatails
    
    * 🐞 fix: mockoon

---
+++ frontend/src/components/CoordinatesMap.tsx
@@ -0,0 +1,49 @@
+import Map, { Marker } from 'react-map-gl/mapbox';
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import { mapboxAccessToken } from '@/constants';
+
+import { useTheme as useMuiTheme } from '@/providers/ThemeProvider.tsx';
+
+import LocationPinIcon from '@mui/icons-material/LocationPin';
+import Box from '@mui/material/Box';
+import { useTheme } from '@mui/material/styles';
+
+interface CoordinatesMapProps {
+  longitude: number;
+  latitude: number;
+}
+
+export const CoordinatesMap = ({ longitude, latitude }: CoordinatesMapProps) => {
+  const { mapboxStyle } = useMuiTheme();
+  const theme = useTheme();
+
+  return (
+    <Box display="flex" justifyContent="center" width="90%" mt={2} height="60vh">
+      <Box width="80%" flexGrow={1}>
+        <Map
+          mapboxAccessToken={mapboxAccessToken}
+          initialViewState={{
+            longitude: 19.0402,
+            latitude: 47.4979,
+            zoom: 12,
+          }}
+          style={{ width: '100%', height: '100%' }}
+          mapStyle={mapboxStyle}
+        >
+          <Marker longitude={longitude} latitude={latitude} anchor="bottom">
+            <LocationPinIcon
+              color="inherit"
+              sx={{
+                fontSize: 42,
+                cursor: 'default',
+                color: theme.palette.primary.marker,
+              }}
+            />
+          </Marker>
+        </Map>
+      </Box>
+    </Box>
+  );
+};
