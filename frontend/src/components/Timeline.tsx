c4aaea5a4b9aab9f5df2df38cd3a6a807961011c
fatgod4937 <dergeczakos1208@gmail.com>
2025-08-01 10:23:56 +0200

    [fix] fixed timeline component according to what I get as a response from backend, added a bit of styling

--- frontend/src/components/Timeline.tsx
+++ frontend/src/components/Timeline.tsx
@@ -5,47 +5,53 @@ import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 import TimelineItem, { timelineItemClasses } from '@mui/lab/TimelineItem';
-import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import Typography from '@mui/material/Typography';
 
-interface TimelineProp {
-  status: {
-    changeDate: string;
-    changeMessage: string;
-    changeLocation: string;
-  }[];
+interface TimelineEvent {
+  id: string;
+  parcelId: string;
+  status: string;
+  description: string;
+  timestamp: string;
 }
 
-export default function TimelineComponent({ status }: TimelineProp) {
+interface TimelineProps {
+  timeline: TimelineEvent[];
+}
+const notSmallScreenStyle = { top: '50%', left: '50%', transform: 'translate(50%, 0)' };
+export default function TimelineComponent({ timeline }: TimelineProps) {
   const isSmallScreen = useSmallScreen();
+
   return (
-    <Timeline sx={{ ...(isSmallScreen && { [`& .${timelineItemClasses.root}:before`]: { flex: 0, padding: 0 } }) }}>
-      {status.map((value) => {
-        return (
-          <TimelineItem key={value.changeMessage}>
-            {!isSmallScreen && (
-              <TimelineOppositeContent color="textSecondary">
-                {new Date(value.changeDate).toLocaleDateString()}
-              </TimelineOppositeContent>
-            )}
-            <TimelineSeparator>
-              <TimelineDot />
-              {value.changeMessage !== 'Delivered' && <TimelineConnector />}
-            </TimelineSeparator>
-            <TimelineContent variant="subtitle2">
-              {value.changeMessage}
-              <br />
-              <Typography variant="caption">{value.changeLocation}</Typography>
-              {isSmallScreen && (
-                <Typography variant="subtitle2" color="textSecondary">
-                  {new Date(value.changeDate).toLocaleDateString()}
-                </Typography>
-              )}
-            </TimelineContent>
-          </TimelineItem>
-        );
-      })}
+    <Timeline
+      sx={{
+        paddingTop: isSmallScreen ? 0 : 7,
+        ...(!isSmallScreen && notSmallScreenStyle),
+        [`& .${timelineItemClasses.missingOppositeContent}:before`]: { display: 'none' },
+        ...(isSmallScreen && {
+          [`& .${timelineItemClasses.root}:before`]: { flex: 0, padding: 0 },
+        }),
+      }}
+    >
+      {timeline.map((event) => (
+        <TimelineItem key={event.id}>
+          <TimelineSeparator>
+            <TimelineDot />
+            {event.status !== 'DELIVERED' && <TimelineConnector />}
+          </TimelineSeparator>
+          <TimelineContent variant="subtitle2" fontSize={12}>
+            {event.description.toUpperCase()}
+            <br />
+            <Typography variant="caption" noWrap>
+              {event.status.replace('_', ' ')}
+            </Typography>
+            <Typography variant="subtitle2" color="textSecondary">
+              {new Date(event.timestamp).toLocaleDateString()}
+            </Typography>
+          </TimelineContent>
+        </TimelineItem>
+      ))}
     </Timeline>
   );
 }
