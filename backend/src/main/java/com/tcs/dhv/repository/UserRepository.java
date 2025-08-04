190bbc70034776602e8316dee411bf9adbf8c866
Tibor Kovacs <tiborkovacs2001@gmail.com>
2025-08-04 14:46:17 +0200

    fix: Added EntityGraph to user and refreshtoken repositories

--- backend/src/main/java/com/tcs/dhv/repository/UserRepository.java
+++ backend/src/main/java/com/tcs/dhv/repository/UserRepository.java
@@ -1,12 +1,17 @@
 package com.tcs.dhv.repository;
 
 import com.tcs.dhv.domain.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    @EntityGraph(attributePaths = {
+            "address",
+            "refreshTokens"
+    })
     Optional<User> findByEmail(final String email);
 
     boolean existsByEmail(final String email);
