0c9d3184f13b1e4f691827ef6120949d5bad8451
Abdelrahman Abdelaal <abdelrahman5abdelaal@gmail.com>
2025-08-11 14:02:42 +0200

    chore: remove unused method from RecipientRepository
    # Conflicts:
    #	backend/src/main/java/com/tcs/dhv/repository/RecipientRepository.java

--- backend/src/main/java/com/tcs/dhv/repository/RecipientRepository.java
+++ backend/src/main/java/com/tcs/dhv/repository/RecipientRepository.java
@@ -3,10 +3,5 @@ package com.tcs.dhv.repository;
 import com.tcs.dhv.domain.entity.Recipient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-import java.util.UUID;
-
-public interface RecipientRepository extends JpaRepository<Recipient, UUID> {
-
-    Optional<Recipient> findByEmail(String email);
+public interface RecipientRepository extends JpaRepository<Recipient, Long> {
 }
