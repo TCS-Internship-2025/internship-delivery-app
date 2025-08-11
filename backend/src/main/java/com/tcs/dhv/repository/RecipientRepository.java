27ae9b8adf0c8f07de134a94cde34cc50cd25cdb
Abdelrahman Abdelaal <abdelrahman5abdelaal@gmail.com>
2025-08-11 14:59:50 +0200

    fix: change RecipientRepository key from Long to UUID

--- backend/src/main/java/com/tcs/dhv/repository/RecipientRepository.java
+++ backend/src/main/java/com/tcs/dhv/repository/RecipientRepository.java
@@ -3,5 +3,7 @@ package com.tcs.dhv.repository;
 import com.tcs.dhv.domain.entity.Recipient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RecipientRepository extends JpaRepository<Recipient, Long> {
+import java.util.UUID;
+
+public interface RecipientRepository extends JpaRepository<Recipient, UUID> {
 }
